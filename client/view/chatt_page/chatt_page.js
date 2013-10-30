f1d51def8a12cef9d5e62e88f1aa562fa8a6f8d3
Idraki Muhamad <idrakimuhamad@gmail.com>
2013-10-30 15:43:43 +0800

    added timestamp and fix the bug
    added timestamp and fix the bug where clearing the input if someone else
    send in new chat.
    Now working with IE8+ , though degrade experience to compare with Chrome
    and the gang.

--- client/view/chatt_page/chatt_page.js
+++ client/view/chatt_page/chatt_page.js
@@ -1,5 +1,6 @@
 Session.setDefault('chatt_name', null);
 Session.set('current_chatter', null);
+//Session.setDefault('new_notification', false);
 
 Template.chatt_page.rendered = function() {
     $(window).scrollTop($(document).height());
@@ -22,6 +23,9 @@ Template.chatt_page.helpers({
         if(dialogs)
             return dialogs;
     },
+    time : function() {
+        return moment(this.timestamp).format('hh:mma');
+    },
 //    not_the_current_chatter : function() {
 //        
 //        if( this.chatter === Session.get('current_chatter') ) {
@@ -33,6 +37,12 @@ Template.chatt_page.helpers({
 //    },
     focus : function() {
         return Session.get('on_focus') ? 'focus' : '';
+    },
+    connected : function() {
+        return Meteor.status().status === "connected" ? '' : 'disable';
+    },
+    not_connected : function() {
+        return Meteor.status().status !== "connected";
     }
 });
 
@@ -51,7 +61,7 @@ Template.chatt_page.events({
         }
     },
     'keyup .chatt-input' : function(e,t) {
-        if(e.which === 13) {
+        if(e.which == 13) {
             e.preventDefault();
             createDialog(t);
         }
@@ -60,6 +70,7 @@ Template.chatt_page.events({
 
 function createDialog(template) {
     var dialog = template.find('.chatt-input').value;
+    $('.chatt-input').val('').select();
             
     if(dialog) {
         Chatt.update(Session.get('current_chatt'), {
@@ -72,8 +83,33 @@ function createDialog(template) {
             }
         }, function(err) {
             if(!err) {
-                $('.chatt-input').val('').select();
+//                Session.set('new_notification', true);
             }
         });
     }
 }
+
+// little hack for blinking title
+//var isOldTitle = true;
+//var oldTitle = "Chatt - A Stupid Chat App";
+//var newTitle = 'New chatt! | Chatt - A Stupid Chat App';
+//var interval = null;
+//
+//
+//
+//Meteor.autorun(function() {
+//    if(Session.get('new_notification')) {        
+//        interval = setInterval(changeTitle, 700);
+//    }
+//});
+//
+//function changeTitle() {
+//    document.title = isOldTitle ? oldTitle : newTitle;
+//    isOldTitle = !isOldTitle;
+//}
+//
+//$(window).focus(function () {
+//    clearInterval(interval);
+//    Session.set('new_notification', false);
+//    $('title').text(oldTitle);
+//});
