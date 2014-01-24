d8687b4785a6efd7e44dec58f31a3406b2c049a6
Idraki Muhamad <idrakimuhamad@gmail.com>
2014-01-24 17:52:31 +0800

    Login Sign Up Styling

--- client/view/home/home.js
+++ client/view/home/home.js
@@ -1,5 +1,6 @@
 
 Session.setDefault('isSignup', true);
+Session.setDefault('isFlip', true);
 
 Template.home.helpers({
 	signup : function () {
@@ -7,6 +8,48 @@ Template.home.helpers({
 	},
 	login : function() {
 		return !Session.get('isSignup');
+	},
+	page_class : function() {
+		return Session.get('isSignup') ? 'signup-page' : 'login-page';
+	}
+});
+
+Template.signup.rendered = function() {
+	if (!this.rendered) {
+		this.rendered = true;
+
+		Meteor.setTimeout(function () {
+			$('.signup').addClass('visible');
+		}, 300);
+	}
+}
+
+Template.login.rendered = function() {
+	if (!this.rendered) {
+		this.rendered = true;
+
+		Meteor.setTimeout(function () {
+			$('.login').addClass('visible');
+		}, 300);
+	}
+}
+
+Template.home.events({
+	'tap .signup-footer .to-login, click .signup-footer .to-login' : function(e,t) {
+		e.preventDefault();
+		$(t.find('.signup')).addClass('flip');
+		// Meteor.setTimeout(function () {
+			Session.set('isSignup', false);
+			// $(t.find('.login')).addClass('flip');
+		// }, 250);
+	},
+	'tap .login-footer .to-signup, click .login-footer .to-signup' : function(e,t) {
+		e.preventDefault();
+		$(t.find('.login')).addClass('flip');
+		// Meteor.setTimeout(function () {
+			Session.set('isSignup', true);
+			// $(t.find('.signup')).addClass('flip');
+		// }, 250);
 	}
 });
 
@@ -17,4 +60,13 @@ Template.signup.events({
 	'blur .form-control' : function(e,t) {
 		$(e.currentTarget).parent('.form-group').removeClass('focus');
 	}
-})
+});
+
+Template.login.events({
+	'focus .form-control' : function(e,t) {
+		$(e.currentTarget).parent('.form-group').addClass('focus');
+	},
+	'blur .form-control' : function(e,t) {
+		$(e.currentTarget).parent('.form-group').removeClass('focus');
+	}
+});
